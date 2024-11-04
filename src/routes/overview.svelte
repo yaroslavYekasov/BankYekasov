d168ab068b175dbdac935f6a59875135e53861bb
fngpr <fngpr.56@gmail.com>
2024-11-04 21:03:57 +0200

    User can see  last operations with different colors

--- src/routes/overview.svelte
+++ src/routes/overview.svelte
@@ -20,24 +20,57 @@
         return await post(`auth/getTransactions`)
     }
 </script>
+
 {#if process.browser}
-{#await getMyData()}
-    Loading...
+    {#await getMyData()}
+        loading...
     {:then my}
-    <section>
-        <p>{my.name}</p>
-    </section>
-    <section>
-        My funds
-        <p>{my.funds}</p>
-    </section>
-    <section>
-        <ul>
-            {#each my.accounts as account}
-                <li>{account.number} {account.name}</li>
-            {/each}
-        </ul>
-    </section>
-    <pre>{JSON.stringify(my, null, 4)}</pre>
-{/await}
-    {/if}
+        <section>
+            <p style="font-size:xx-large">{my.name}</p>
+        </section>
+        <section>
+            My funds
+            <p style="font-size:xx-large; color: {my.funds >= 0 ? 'green' : 'red'}">{my.funds}</p>
+        </section>
+        <section>
+            <ul>
+                {#each my.accounts as account}
+                    <li>{account.number} {account.name}</li>
+                {/each}
+            </ul>
+        </section>
+        <section>
+            {#await getTransactions()}
+                Loading...
+            {:then transactions}
+
+                <table class="table table-striped table-bordered">
+                    <thead>
+                    <tr>
+                        <th>SenderName</th>
+                        <th>Amount</th>
+                        <th>CreateAt</th>
+                        <th>Status</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {#each transactions as transaction}
+                        <tr>
+                            <td><b>{transaction.senderName}</b><br>{transaction.explanation}</td>
+                            {#if transaction.amount > 0}
+                                <td class="text-success">{transaction.amount} {transaction.currency}</td>
+                            {:else}
+                                <td class="text-danger">{transaction.amount} {transaction.currency}</td>
+                            {/if}
+                            <td>{transaction.createAt}</td>
+                            <td>{transaction.status}<br>{transaction.statusDetail}</td>
+                            <td>{transaction.loggedInUser}</td>
+                        </tr>
+                    {/each}
+                    </tbody>
+                </table>
+
+            {/await}
+        </section>
+    {/await}
+{/if}
